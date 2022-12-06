e61881d24b0ac018b739b9e77196480b59222b9d
Yeison Garzón T <garzontoroyeison@gmail.com>
2022-12-05 22:01:59 -0500

    feat: wpp button added

--- src/layouts/LayoutDefault.js
+++ src/layouts/LayoutDefault.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import Footer from '../components/layout/Footer';
+import { FloatingWhatsApp } from 'react-floating-whatsapp'
 
 const LayoutDefault = ({ children }) => (
   <>
@@ -8,6 +9,13 @@ const LayoutDefault = ({ children }) => (
     <main className="site-content">
       {children}
     </main>
+    <FloatingWhatsApp 
+        phoneNumber="+573177837117"
+        accountName="Foo"
+        allowEsc
+        allowClickAway
+        notification
+        notificationSound />
     <Footer />
   </>
 );
